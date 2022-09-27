981b7dadf619ba0d3a71c878481aa84609167f5c
marvinpieper <marvinpieper@protonmail.com>
2022-09-27 19:31:30 +0200

    add some comments

--- time_track.py
+++ time_track.py
@@ -29,9 +29,20 @@ def track():
     global counter 
     counter = counter + 1
     if counter % 2 == 1:
+
+        # compute working time
         global start_time 
         start_time = time_stamp
+
+        # change button text
         btn['text'] = 'End'
+
+        # unhide label
+        lab.pack()
+
+        # hide quit button
+        quit.pack_forget()
+
     else:
         # calculate the duration
         end_time = time_stamp
@@ -39,13 +50,29 @@ def track():
 
         # insert a new line in the file
         f.write(str(duration) + '\n')
+
+        # change button text
         btn['text'] = 'Start'
 
+        # hide label
+        lab.pack_forget()
+
+        # unhide quit button
+        quit.pack()
+
     f.close()
+
+
+# show label while the time is recording
+lab = tk.Label(frame, text="time is running ...")
 
 # Start/End Button
 btn = tk.Button(frame, text = "Start", command = track)
 btn.pack()
+
+# quit button 
+quit = tk.Button(frame, text="Quit", command=frame.destroy)
+quit.pack()
 
 # main loop
 frame.mainloop()
