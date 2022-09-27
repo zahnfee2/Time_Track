11d2c62a92dddc6819c9e041f870141341012cfc
marvinpieper <marvinpieper@protonmail.com>
2022-09-27 20:37:27 +0200

    add Date picker. The picked time will not saved in the csv file yet!

--- time_track.py
+++ time_track.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from datetime import datetime
 from os.path import exists
+from tkcalendar import Calendar
 
 # Counter to identify start and end
 counter = 0
@@ -9,7 +10,7 @@ start_time = datetime.now()
 # Top level window
 frame = tk.Tk()
 frame.title("TextBox Input")
-frame.geometry('200x100')
+frame.geometry('600x400')
 
 # write the timestamp in a file
 def track():
@@ -17,7 +18,10 @@ def track():
     # create timestamp
     time_stamp = datetime.now()
 
+    # filename
     filename = "working_time.csv"
+
+    # check if file exists 
     if not exists(filename):
         # open file
         f = open(filename, "a")
@@ -74,6 +78,10 @@ def track():
 
     f.close()
 
+# gettin date from the calendar
+def fetch_date():
+    date.config(text = "Selected Date is: " + tkc.get_date())
+
 
 # show label while the time is recording
 lab = tk.Label(frame, text="time is running ...")
@@ -81,6 +89,16 @@ lab = tk.Label(frame, text="time is running ...")
 # Start/End Button
 btn = tk.Button(frame, text = "Start", command = track)
 btn.pack()
+
+
+# Calendar picker
+tkc = Calendar(frame,selectmode = "day",year=2022,month=10,date=1)
+tkc.pack()
+but = tk.Button(frame,text="Select Date",command=fetch_date, bg="black", fg='white')
+but.pack()
+
+date = tk.Label(frame, text="")
+date.pack()
 
 # quit button 
 quit = tk.Button(frame, text="Quit", command=frame.destroy)
