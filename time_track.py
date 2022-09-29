df20ec8b04eb3ab384ff77998e503a40f1ebca5a
marvinpieper <marvinpieper@protonmail.com>
2022-09-29 22:21:27 +0200

    new termin can be added and the list will sort by the startdate

--- time_track.py
+++ time_track.py
@@ -1,11 +1,18 @@
 import tkinter as tk
 from datetime import datetime
 from os.path import exists
-import string
+from turtle import st
+import pandas as pd
 
 # Counter to identify start and end
 counter = 0
+
+# time format 
+time_format = '%Y/%m/%d %H:%M'
+
+# chreate start time 
 start_time = datetime.now()
+start_time = start_time.strftime(time_format)
 
 # Top level window
 frame = tk.Tk()
@@ -15,7 +22,6 @@ frame.geometry('600x300') # change window size
 # filename
 filename = "working_time.csv"
 
-time_format = '%Y/%m/%d %H:%M'
 
 # save string in file
 def save_in_file(content, filename, operation):
@@ -39,7 +45,7 @@ def track():
 
     # check if file exists 
     if not exists(filename):
-        header = "start;end;duration\n"
+        header = "start,end,duration\n"
         save_in_file(header, filename, 'a')
 
     # check if start or end
@@ -68,6 +74,13 @@ def track():
         # show save button
         save_button.pack()
 
+        # hide add time
+        add_start_time_lbl.pack_forget()
+        add_end_time_lbl.pack_forget()
+        add_start_time.pack_forget()
+        add_end_time.pack_forget()
+        add_time_save.pack_forget()
+
     else: # is not running
         # calculate the duration
         global end_time
@@ -76,7 +89,7 @@ def track():
         duration = convert_to_datetime(end_time) - convert_to_datetime(start_time)
 
         # create line 
-        str_data = str(start_time) + ";" + str(end_time) + ";" + str(duration) + '\n'
+        str_data = str(start_time) + "," + str(end_time) + "," + str(duration) + '\n'
 
         # insert a new line in the file
         save_in_file(str_data, filename, 'a')
@@ -97,6 +110,18 @@ def track():
         # hide save button
         save_button.pack_forget()
 
+        # show add time 
+        add_start_time_lbl.pack()
+        add_end_time_lbl.pack()
+        add_start_time.pack()
+        add_start_time.insert('1.0', str(start_time))
+        add_end_time.pack()
+        add_end_time.insert('1.0', str(start_time))
+        add_time_save.pack()
+
+        # hide saved label
+        saved_lbl.pack_forget()
+
 def convert_to_datetime(str_datetime):
     return datetime.strptime(str_datetime, time_format)
 
@@ -116,45 +141,45 @@ def save_correct_start():
     global start_time
     start_time_old = start_time
     start_time = str(change_start_time.get(1.0, 'end-1c'))
-    print("Start_time is changend from: " + start_time_old + " to: " + start_time)
-    # save_new_data_and_sort_list(datetime)
+    saved_lbl.pack()
 
 
 
-def insert_datetime(content, datetime_str):
-    print(content)
-    content.append(datetime_str)
-    # data = sorted(content, key=lambda row: datetime.strptime(row[0], time_format))
+
+def sort_list_by_datetime(content):#
+    data = sorted(content, key=lambda row: datetime.strptime(row[0], time_format))
     print("sort done")
-    # return data
+    return data
 
-"""
-conv_datetime = convert_to_datetime(datetime_str)
-    conv_content = []
-
-    for i in content:
-        split_i = i.split(';')
-        if(len(split_i) == 3):
-            counter = 0
-            for letter in split_i:
-                if(counter == 0):
-                    conv_i = convert_to_datetime(letter)
-                    conv_content.append(conv_i)
-                    break
-"""
     
+def save_time():
+    new_start_time = add_start_time.get(1.0, 'end-1c')
+    new_end_time = add_end_time.get(1.0, 'end-1c')
+    new_duration = convert_to_datetime(new_end_time) - convert_to_datetime(new_start_time)
 
+    line = {'start': str(new_start_time) 
+    , 'end': str(new_end_time) 
+    , 'duration': str(new_duration)}
     
 
 
-# save datetime in file and sort the list 
-def save_new_data_and_sort_list(datetime):
-    content = get_file_content(filename)
-    for i in content:
+    content = pd.read_csv(filename)
+    content = pd.DataFrame(content)
+    content = content.append(line, ignore_index=True)
+    print(content.head())
+    content['start'] = pd.to_datetime(content.start, infer_datetime_format = True)
+    content.sort_values(by='start', ascending=True, inplace=True)
+
+    content.to_csv(filename, index=False)
+    print(content.head())
+
+    print("List was sorted!")
+
+
+# print a list on the consol 
+def print_list(list):
+    for i in list:
         print(i)
-    # sort_content = insert_datetime(content, datetime)
-    # print(sort_content)
-    # save_in_file(sort_content, filename, 'w')
 
 
 
@@ -166,14 +191,34 @@ lab = tk.Label(frame, text="time is running ...")
 btn = tk.Button(frame, text = "Start", command = track)
 btn.pack()
 
+# forgot label
+forgot_lbl = tk.Label(frame,text="Did you forget to press start?\n  Here you have the possibility to correct the start time.")
+
+saved_lbl = tk.Label(frame,text="Saved!")
+
 # change the start time
 change_start_time = tk.Text(frame, height=2, width=30)
 
-# forgot label
-forgot_lbl = tk.Label(frame, text="Did you forget to press start?\n  Here you have the possibility to correct the start time.")
-
 # save button
 save_button = tk.Button(frame, text="Save", command=save_correct_start)
+
+# add start time
+add_start_time_lbl = tk.Label(frame, text="Start Time:")
+add_start_time_lbl.pack()
+add_start_time = tk.Text(frame, height=2, width=30)
+add_start_time.insert('1.0', str(start_time))
+add_start_time.pack()
+
+# add end time
+add_end_time_lbl = tk.Label(frame, text="End Time:")
+add_end_time_lbl.pack()
+add_end_time = tk.Text(frame, height=2, width=30)
+add_end_time.insert('1.0', str(start_time))
+add_end_time.pack()
+
+# save button to add new time 
+add_time_save = tk.Button(frame, text="save", command=save_time)
+add_time_save.pack()
 
 # quit button 
 quit = tk.Button(frame, text="Quit", command=frame.destroy)
