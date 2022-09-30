7f6d1e8f3d1c8c044427161118fca0bb31d8aa40
Tooth-fairy <marvinpieper@protonmail.com>
2022-09-30 14:22:05 +0200

    dont know what happend here

--- time_track.py
+++ time_track.py
@@ -9,10 +9,10 @@ import pandas as pd
 # Counter to identify start and end
 counter = 0
 
-# time format 
+# time format
 time_format = '%Y/%m/%d %H:%M'
 
-# chreate start time 
+# chreate start time
 start_time = datetime.now()
 start_time = start_time.strftime(time_format)
 
@@ -24,8 +24,8 @@ frame.geometry('600x300') # change window size
 # filename
 filename = "working_time.csv"
 
-# coumpute the duration 
-def get_entire_duration(): 
+# coumpute the duration
+def get_entire_duration():
     content = pd.read_csv(filename)
     content = pd.DataFrame(content)
     content['duration'] = pd.to_timedelta(content.duration)
@@ -38,7 +38,7 @@ def save_in_file(content, filename, operation):
 
     # write content in fiel
     f.write(str(content))
-    
+
     # close file
     f.close()
 
@@ -51,18 +51,18 @@ def track():
     time_stamp = time_stamp.strftime(time_format)
 
 
-    # check if file exists 
+    # check if file exists
     if not exists(filename):
         header = "start,end,duration\n"
         save_in_file(header, filename, 'a')
 
     # check if start or end
-    global counter 
+    global counter
     counter = counter + 1
     if counter % 2 == 1: # is running
 
         # compute working time
-        global start_time 
+        global start_time
         start_time = time_stamp
 
         # change button text
@@ -96,7 +96,7 @@ def track():
 
         duration = convert_to_datetime(end_time) - convert_to_datetime(start_time)
 
-        # create line 
+        # create line
         str_data = str(start_time) + "," + str(end_time) + "," + str(duration) + '\n'
 
         # insert a new line in the file
@@ -111,14 +111,14 @@ def track():
         # unhide quit button
         quit.pack()
 
-        # hide correct button 
+        # hide correct button
         forgot_lbl.pack_forget()
         change_start_time.pack_forget()
 
         # hide save button
         save_button.pack_forget()
 
-        # show add time 
+        # show add time
         add_start_time_lbl.pack()
         add_end_time_lbl.pack()
         add_start_time.pack()
@@ -138,13 +138,13 @@ def get_file_content(filename):
     content = []
     with open(filename, 'r') as f:
         f.readline()
-        for line in f: 
+        for line in f:
             content.append(str(line))
 
     return content
 
 
-# save date time in file 
+# save date time in file
 def save_correct_start():
     global start_time
     start_time_old = start_time
@@ -159,16 +159,16 @@ def sort_list_by_datetime(content):#
     print("sort done")
     return data
 
-    
+
 def save_time():
     new_start_time = add_start_time.get(1.0, 'end-1c')
     new_end_time = add_end_time.get(1.0, 'end-1c')
     new_duration = convert_to_datetime(new_end_time) - convert_to_datetime(new_start_time)
 
-    line = {'start': str(new_start_time) 
-    , 'end': str(new_end_time) 
+    line = {'start': str(new_start_time)
+    , 'end': str(new_end_time)
     , 'duration': str(new_duration)}
-    
+
 
 
     content = pd.read_csv(filename)
@@ -184,7 +184,7 @@ def save_time():
     print("List was sorted!")
 
 
-# print a list on the consol 
+# print a list on the consol
 def print_list(list):
     for i in list:
         print(i)
@@ -224,14 +224,14 @@ add_end_time = tk.Text(frame, height=2, width=30)
 add_end_time.insert('1.0', str(start_time))
 add_end_time.pack()
 
-# save button to add new time 
+# save button to add new time
 add_time_save = tk.Button(frame, text="Save", command=save_time)
 add_time_save.pack()
 
 entire_duration_label = tk.Label(frame, text="Entire working duration: " + str(get_entire_duration()))
 entire_duration_label.pack()
 
-# quit button 
+# quit button
 quit = tk.Button(frame, text="Quit", command=frame.destroy)
 quit.pack()
 
