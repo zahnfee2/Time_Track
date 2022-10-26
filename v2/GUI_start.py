505a3ab7b69fe90bd16b28f8b1766ee50e00a431
marvin <marvinpieper@protonmail.com>
2022-10-26 21:58:17 +0200

    Gui is reade with start and end button. Time will save correctly in csv file

--- v2/GUI_start.py
+++ v2/GUI_start.py
@@ -1,16 +1,19 @@
 import tkinter
-from pathlib import Path
 from Time_track import *
 from datetime import datetime
+from Handle_files import *
+from include import *
+
 
 class UI_Start():
     def __init__(self):
         self.root = tkinter.Tk()
         self.root.title("Time Track")
-        self.root.geometry('300x400')
+        self.root.geometry('300x200')
 
-        tkinter.Label(self.root, text="Welcome to daily.").pack()
-        tkinter.Button(self.root, text="Open", height=1, width=15, command=lambda: UI_Track_Time() ).pack(pady=2)
+        tkinter.Label(self.root, text="Welcome to daily.").pack(pady=20)
+        tkinter.Button(self.root, text="Track Time", height=1, width=15, command=UI_Track_Time ).pack(pady=4)
+        tkinter.Button(self.root, text="Show recordet time", height=1, width=15, command=UI_Show_Rec_Time ).pack(pady=4)
 
         self.root.mainloop()
 
@@ -18,29 +21,105 @@ class UI_Start():
         self.root.destroy()
 
 
+
 class UI_Track_Time():
     def __init__(ui_track):
+
+        # Main Window
         ui_track.win_track = tkinter.Tk()
         ui_track.win_track.title("Time Track")
-        ui_track.win_track.geometry('200x200')
+        ui_track.win_track.geometry('300x200')
 
-        tkinter.Label(ui_track.win_track, text="Welcome to daily.").pack()
-        tkinter.Button(ui_track.win_track, text="See entries", height=1, width=15, command=start_tracking ).pack(pady=2)
+        # End Button
+        ui_track.end_button = tkinter.Button(ui_track.win_track, text="End Tracking", height=1, width=15, command=ui_track.end_tracking )
 
-        tkinter.Button(ui_track.win_track, text="Quit", height=1, width=15, command=quit ).pack()
+        # Topic Text
+        ui_track.topic_label = tkinter.Label(ui_track.win_track, text="Topic:")
+        ui_track.topic_Text = tkinter.Text(ui_track.win_track, height=2, width=30)
+
+        # Start Button
+        ui_track.start_button = tkinter.Button(ui_track.win_track, text="Start Tracking", height=1, width=15, command=ui_track.start_tracking )
+        ui_track.start_button.pack(pady=2)
+
+        # Quit Button
+        ui_track.quit_button = tkinter.Button(ui_track.win_track, text="Quit", height=1, width=15, command=quit )
+        ui_track.quit_button.pack(pady=2)
+
+        # Main Loop
         ui_track.win_track.mainloop()
     
+
+    def compute_duration(ui_track):
+        ui_track.duration = ui_track.end_time - ui_track.start_time
+
+
+    def start_tracking(ui_track):
+        ui_track.start_time = datetime.now()
+
+        # convert time in a good format to save the data
+        # ui_track.start_time = start_time.strftime(time_format)
+
+        # hide start button
+        ui_track.start_button.pack_forget()
+
+        # hide quit button
+        ui_track.quit_button.pack_forget()
+
+        # show end button
+        ui_track.end_button.pack(pady=2)
+
+        # show topic Textfild
+        ui_track.topic_label.pack()
+        ui_track.topic_Text.pack(pady=2)
+
+
+    def end_tracking(self):
+        self.end_time = datetime.now()
+        self.topic = self.topic_Text.get(1.0, 'end-1c')
+
+        # show start button
+        self.start_button.pack(pady=2)
+
+        # show quit button
+        self.quit_button.pack(pady=2)
+
+        # hide end button
+        self.end_button.pack_forget()
+
+        # hide topic Textfile
+        self.topic_label.pack_forget()
+        self.topic_Text.delete("1.0", "end")
+        self.topic_Text.pack_forget()
+        
+        self.compute_duration()
+        self.save_time()
+
+
+    def save_time(self):
+        start_time_str = self.start_time.strftime(time_format)
+        end_tim_str = self.end_time.strftime(time_format)
+        duration_str = str(self.duration)
+        row = [start_time_str, end_tim_str, duration_str, self.topic]
+        write_in_csv_file(row)
+        
+
+
+
     def quit(ui_track):
         ui_track.win_track.destroy()
 
 
-def start_tracking():
 
-    time_format = '%Y/%m/%d %H:%M'
-    start_time = datetime.now()
-    start_time = start_time.strftime(time_format)
-    end_time = datetime.now()
-    end_time = end_time.strftime(time_format)
-    t = Time_track(start_time)
-    print(t)
+class UI_Show_Rec_Time():
+    def __init__(self):
+        self.root = tkinter.Tk()
+        self.root.title("Time Track")
+        self.root.geometry('300x400')
+
+        tkinter.Label(self.root, text="This is your recordet Time").pack(pady=10)
+
+        self.root.mainloop()
+
+    def quit(self):
+        self.root.destroy()
 
--- v2/Handle_files.py
+++ v2/Handle_files.py
@@ -1,3 +1,6 @@
+from include import csv_path
+import os.path
+import csv
 
 def write_in_file(filename, content, operator):
     with open(filename, operator) as f:
@@ -7,3 +10,15 @@ def write_in_file(filename, content, operator):
 def get_content_of_file(filename):
     with open(filename, 'w') as f:
         content = f.read()
+    return content
+
+
+def write_in_csv_file(row):
+    f = open(csv_path, 'a')
+    writer = csv.writer(f)
+    if(os.path.isfile(csv_path)):
+        first_line = ['start', 'end', 'duration', 'topic']
+        writer.writerow(first_line)
+    writer.writerow(row)
+
+
